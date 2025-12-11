5c69b9e67ff643b3adcf6ca72d3b5dbefd8065d6
s.voye <s.voye@otec.de>
2025-12-11 14:55:05 +0100

    feat: Add STATIC_ROOT and STATICFILES_FINDERS to settings

--- kanbanapi/kanbanapi/settings.py
+++ kanbanapi/kanbanapi/settings.py
@@ -145,6 +145,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
